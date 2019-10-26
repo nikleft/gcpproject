7a5300d7147f39d6c0ddb658ecb025654cf279e5
Maksym <Maksym_Shumovych@epam.com>
2019-10-27 02:12:29 +0300

    [dev] Added start site template

--- src/main/java/shumovych/gcp/StartApplication.java
+++ src/main/java/shumovych/gcp/StartApplication.java
@@ -2,17 +2,17 @@ package shumovych.gcp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 @RestController
 public class StartApplication {
-
-    @RequestMapping("/")
-    public String home() {
-        return "Hello Docker World";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class, args);
